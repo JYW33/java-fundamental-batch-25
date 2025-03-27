87743742bf0f89891c4e0cd2b2760fbd97c3ccbc
yngwie <yngwiejmantiri@gmail.com>
2025-03-27 20:15:52 +0800

    first commit

---
+++ src/main/java/com/juaracoding/Main.java
@@ -0,0 +1,18 @@
+package com.juaracoding;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("JuaraCoding");
+        int x = 10;
+
+        if (true){
+            System.out.println("Statement IF");
+            System.out.println(x);
+        }
+
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+            System.out.println(x);
+        }
+    }
+}
